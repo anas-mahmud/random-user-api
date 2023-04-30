62e49b8b372c518e2d207ba389f94b7176144b84
Anas <anasmahmud523@gmail.com>
2023-04-30 12:14:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const userRoutes = require("./routes/user.route");
+const viewCount = require("./middleware/viewCount");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use(viewCount);
+
+app.use("/api/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("random user api is running");
+});
+
+app.all("*", (req, res) => {
+  res.send("No routes found");
+});
+
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
---
+++ routes/user.route.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const userControllers = require("../controllers/usersController");
+
+const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.send("user found");
+// });
+
+// router.post("/", (req, res) => {
+//   res.send("user added");
+// });
+
+router
+  .route("/random")
+  /**
+   * @api {get} /a random user
+   * @apiDescription Get a random user
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get(userControllers.getUserRandomly);
+
+router
+  .route("/all")
+  /**
+   * @api {get} /all the users
+   * @apiDescription Get all the users
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get(userControllers.getUsers)
+  /**
+   * @api {get} /add user
+   * @apiDescription save/add a user
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .post(userControllers.addUser);
+
+router
+  .route("/:id")
+  /**
+   * @api {get} /a user
+   * @apiDescription get a user by id
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get(userControllers.getUserById)
+  /**
+   * @api {get} /update user
+   * @apiDescription update a user by id
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .patch(userControllers.updateUserById)
+  /**
+   * @api {get} /delete user
+   * @apiDescription delete a user by id
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .delete(userControllers.deleteUser);
+
+module.exports = router;
